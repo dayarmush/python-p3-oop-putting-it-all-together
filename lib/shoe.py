d1adec42b5ebdd3a2a99f1f1a4152236ab2dd02e
Dovid Yarmush <yarmushda@gmail.com>
2023-06-14 15:26:49 -0400

    shoe class complete, passing all tests

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    def __init__(self, brand, size):
+    def __init__(self, brand, size = 0):
         self.brand  = brand
         self.size = size
+        self.condition = ''
 
+    @property
+    def size(self):
+        return self._size
         
+    @size.setter
+    def size(self, size):
+        if type(size) == int:
+            self._size = size
+        else:
+            return print("size must be an integer")
+        
+    def cobble(self):
+        self.condition = 'New'
+        return print('Your shoe is as good as new!')
+    
+    # def condition(self):
+    #     return print('New!')
