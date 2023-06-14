ca62b28be77266bd01f60f39f5df0caa77b81420
Dovid Yarmush <yarmushda@gmail.com>
2023-06-14 14:40:45 -0400

    finished book class, passing book tests

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count = 0):
+        self.title = title
+        self.page_count = page_count
+
+    def set_page(self, page):
+        if type(page) == int:
+            self._page_count = page
+        else:
+            print('page_count must be an integer')
+
+    def get_page(self):
+        return self._page_count
+    
+    page_count = property(get_page, set_page)
+
+    def turn_page(self):
+        return print("Flipping the page...wow, you read fast!")
+        
     
         
